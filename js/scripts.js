60a77b7faba86ae6104b19cb747112f75de795e7
Stine Hansen <stinehansen@Stines-MacBook-Pro.local>
2015-02-27 12:35:54 -0800

    fix JS dead end issue with TYPES

--- js/scripts.js
+++ js/scripts.js
@@ -8,33 +8,27 @@ $(window).load(function(){
 //HIDE AND REVEAL MENU ITEMS BY TYPE
 
 $('div.types a.raw').click(function(){
-  
   $('section.item').hide();
-  $('section.raw').show();
+  $('section.raw').show(5000);
+   $('section.item').show(15000);
+  
+ 
   });
 
 
 $('div.types a.veg').click(function(){
   
-  $('section.item').hide();
-  $('section.veg').show();
+ $('section.item').hide();
+  $('section.veg').show(3000);
+   $('section.item').show(9000);
   });
 
 
-$('div.types a.spicy').click(function(){
+ $('div.types a.spicy').click(function(){
   
-  $('section.item').hide();
-  $('section.spicy').show();
+   $('section.item').hide();
+  $('section.spicy').show(5000);
+   $('section.item').show(15000);
 
 
-  });
-$('div.types a.spicy').click(function(){
-  
-  
-  $('section.spicy').hide();
-  $('section.item').show();
-  
-
-  });
-
-
+   });
